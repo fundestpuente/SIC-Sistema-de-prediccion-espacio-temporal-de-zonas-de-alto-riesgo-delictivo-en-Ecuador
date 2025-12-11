af3699cdc986ae134ad908d6bc3eee065fdc7938
Joseph Carrera <jecarrer@espol.edu.ec>
2025-12-10 21:23:32 -0500

    add: seleccion de features y guardado del dataset

--- preprocesamiento_datos_entrenamiento.py
+++ preprocesamiento_datos_entrenamiento.py
@@ -16,3 +16,29 @@ df_union = df_apre.merge(
     how="outer",
     suffixes=("_apre", "_911")
 )
+
+
+# Eliminar franja_horaria porque no contiene datos importantes
+if "franja_horaria" in df_union.columns:
+    df_union.drop(columns=["franja_horaria"], inplace=True)
+
+# features
+features = [
+    "lat_grid", "lon_grid",
+    "mes", "dia", "dia_semana",
+    "franja_horaria",
+    "conteo_delitos",
+    "conteo_delitos_graves",
+    "conteo_llamadas_riesgo"
+]
+
+# asegurarse de que las columnas existan
+features = [c for c in features if c in df_union.columns]
+
+df_final = df_union[features].copy()
+
+# guardar dataset final
+df_final.to_csv("dataset_entrenamiento_final.csv", index=False)
+print("dataset de entramiento guardado: dataset_entranamiento_final.csv")
+print("Registros totales:", len(df_final))
+print("Columnas:", df_final.columns.tolist())
